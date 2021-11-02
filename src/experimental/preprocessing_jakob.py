0eec36c676b5a06885cc16a4c946d309893a22de
Jakob Friedl <if20b089@technikum-wien.at>
2021-11-02 18:05:28 +0100

    Read in Monkey_data Stack Traces and created single CSV file

--- src/experimental/preprocessing_jakob.py
+++ src/experimental/preprocessing_jakob.py
@@ -6,28 +6,47 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
 
+import requests
+
+
 def get_data():
     # GITHUB-ISSUES
     # github_issues_url = "https://github.com/tingsu/DroidDefects/blob/master/ground-truth-cases/Dataset_crashanalysis/Github_issues.csv?raw=true"
     # df_github = pd.read_csv(github_issues_url)
     # print(df_github[['Pkg name', 'Exception name']])
 
-    # MONKEY_DATA
-    monkey_data_url = "https://github.com/tingsu/DroidDefects/blob/master/ground-truth-cases/Dataset_crashanalysis" \
-                      "/Monkey_data.csv?raw=true "
-    df_monkey = pd.read_csv(monkey_data_url)
+    # READ MONKEY_DATA + ADD STACK TRACES
+    # monkey_data_url = "https://github.com/tingsu/DroidDefects/blob/master/ground-truth-cases/Dataset_crashanalysis" \
+    #                   "/Monkey_data.csv?raw=true "
+    # df_monkey = pd.read_csv(monkey_data_url)
+    # monkey_stack_traces = []
+    # counter = 0
+    # s = requests.Session()
+    # for col, item in df_monkey.iterrows():
+    #     folder = item[0]
+    #     bug_report = item[4]
+    #     url = f"https://github.com/tingsu/DroidDefects/blob/master/ground-truth-cases/Dataset_crashanalysis/Monkey_data/{folder}/unique/{bug_report}?raw=true"
+    #
+    #     get_stack_traces(s, monkey_stack_traces, url)
+    #
+    #     counter += 1
+    #     print(counter)
+    #
+    # df_stack_traces = pd.DataFrame({'Stack Trace': monkey_stack_traces})
+    #
+    # df_monkey = df_monkey.join(df_stack_traces)
+    #
+    # print(df_monkey.head(5))
+    #
+    # df_monkey.to_csv(path_or_buf='full_monkey.csv')
 
-    print(df_monkey[['Pkg name', 'Exception name']])
+    test = pd.read_csv('full_monkey.csv')
+    print(test)
 
-    for col, item in df_monkey.iterrows():
-        folder = item[0]
-        bug_report = item[4]
-        file = f"https://github.com/tingsu/DroidDefects/blob/master/ground-truth-cases/Dataset_crashanalysis/Monkey_data/{folder}/unique/{bug_report}"
-
-        # Problem: StackTrace not directly in CSV, but only in sub-folders of repository
-
-        # print(file)
-
+def get_stack_traces(session, df, url):
+    stack_trace = session.get(url)
+    df.append(stack_trace.text.replace('\n', ' '))
+    return df
 
 def process_text(file):
     words = word_tokenize(file)
@@ -54,7 +73,7 @@ def process_text(file):
 
 
 if __name__ == "__main__":
-    # get_data()
-    example_file = "java.lang.RuntimeException: Unable to resume activity {net.etuldan.sparss/net.etuldan.sparss.activity.HomeActivity}: java.lang.NullPointerException: Attempt to invoke virtual method 'void net.etuldan.sparss.adapter.DrawerAdapter.a(int)' on a null object reference"
-    print(f"Original file {example_file}\n")
-    process_text(example_file)
+    get_data()
+    # example_file = "java.lang.RuntimeException: Unable to resume activity {net.etuldan.sparss/net.etuldan.sparss.activity.HomeActivity}: java.lang.NullPointerException: Attempt to invoke virtual method 'void net.etuldan.sparss.adapter.DrawerAdapter.a(int)' on a null object reference"
+    # print(f"Original file {example_file}\n")
+    # process_text(example_file)

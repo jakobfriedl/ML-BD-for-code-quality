534a41c1ffc11d18f4bdf0e4afce49c51c94db3d
Philipp Haider <if20b097@technikum-wien.at>
2022-12-29 19:12:25 +0100

    GUI Functions

--- src/ml-bd-for-code-quality/inno3/lib.py
+++ src/ml-bd-for-code-quality/inno3/lib.py
@@ -1,17 +1,29 @@
 import re
+from src.experimental.supervised.classifiers import rfc, svm, mlp
 from string import punctuation
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from sklearn.ensemble import RandomForestClassifier
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
 from experimental.transformers.special_characters import characters
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.neural_network import MLPClassifier
 
 
-## Preprocessing
+
+# Preprocessing
 def replace_special_characters(words):
     return [characters[w] if w in characters else w for w in words]
+
 
 def clean_text(words):
     # Split words at punctuation
@@ -71,20 +83,120 @@ def word_embedding(df, data_col):
     return v.fit_transform(df[data_col])
 
 ## KMeans
-def k_means(tf_idf, n_clusters, transformer):
+def k_means_gui(tf_idf, n_clusters):
     km = KMeans(n_clusters, random_state=1)
     model = km.fit(tf_idf)
     return model.predict(tf_idf)
 
-## Random Forest Classifier
-def rfc(features, labels, test_size, estimators, max_depth, transformer):
+## Random Forest Classifier 
+def rfc_gui(features, labels, test_size, estimators, max_depth):
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=test_size)
+    rfc = RandomForestClassifier(n_estimators=estimators, max_depth=max_depth)
+    rfc.fit(X_train, y_train)  # Train model using training sets
+    acc_score = accuracy_score(y_test, rfc.predict(X_test))
+    rfc_score = rfc.score(X_test, y_test)
+    return(acc_score, rfc_score)
+
+
+def rfc_gui_transformer(features, labels, test_size, estimators, max_depth):
     print("TODO")
 
 ## Support Vector Machine
-def svm(features, labels, test_size, kernel, transformer):
+def svm_gui(features, labels, test_size, kernel):
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=test_size)
+    svm = SVC(kernel=kernel)
+    svm.fit(X_train, y_train)
+    acc_score = accuracy_score(y_test, svm.predict(X_test))
+    svm_score = svm.score(X_test, y_test)
+    return(acc_score, svm_score)
+
+
+def svm_gui_transformer(features, labels, test_size, kernel):
     print("TODO")
 
-## Neural Network
-def mlp(features, labels, test_size, pca_components, neurons, hidden_layer, transformer):
+## Neural Network# rfc(start, X_train, X_test, y_train, y_test)
+def mlp_gui(features, labels, test_size, pca_components, neurons, hidden_layer):
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=test_size)
+
+    # Data scale to have 0 mean and 1 variance
+    scaler = MaxAbsScaler()
+    X_train = scaler.fit_transform(X_train)
+
+    pca = PCA(n_components=pca_components)
+
+    # Applying the dimensionality reduction
+    # toarray() to convert to a dense numpy array
+    pca.fit(X_train.toarray())
+    X_train = pca.fit_transform(X_train.toarray())
+    pca.fit(X_test.toarray())
+    X_test = pca.transform(X_test.toarray())
+
+    # PCA Plots
+    # 2D Plot
+    if pca.n_components == 2:
+        Xax = X_train[:, 0]
+        Yax = X_train[:, 1]
+        fig, ax = plt.subplots(figsize=(7, 5))
+        fig.patch.set_facecolor('white')
+        for l in np.unique(y_train):
+            ix = np.where(y_train == l)
+            ax.scatter(Xax[ix], Yax[ix])
+
+        plt.xlabel("First Principal Component", fontsize=14)
+        plt.ylabel("Second Principal Component", fontsize=14)
+        plt.legend()
+        plt.show()
+
+    # 3D plot
+    if pca.n_components == 3:
+        Xax = X_train[:, 0]
+        Yax = X_train[:, 1]
+        Zax = X_train[:, 2]
+        fig = plt.figure(figsize=(7, 5))
+        ax = fig.add_subplot(111, projection='3d')
+
+        fig.patch.set_facecolor('white')
+        for l in np.unique(y_train):
+            ix = np.where(y_train == l)
+            ax.scatter(Xax[ix], Yax[ix], Zax[ix])
+
+        ax.set_xlabel("First Principal Component", fontsize=14)
+        ax.set_ylabel("Second Principal Component", fontsize=14)
+        ax.set_zlabel("Third Principal Component", fontsize=14)
+        ax.legend()
+        plt.show()
+
+    # Plotting the evaluation of the number of components
+    plt.rcParams["figure.figsize"] = (18, 6)
+    fig, ax = plt.subplots()
+    arange_limit = pca.n_components + 1
+    xi = np.arange(1, arange_limit, step=1)
+    y = np.cumsum(pca.explained_variance_ratio_)
+    plt.ylim(0.0, 1.1)
+    plt.plot(xi, y, marker='o', linestyle='--', color='b')
+
+    plt.xlabel('Number of Components')
+    plt.xticks(np.arange(0, pca.n_components + 5, step=pca.n_components / 10))
+    plt.ylabel('Cumulative variance (%)')
+    plt.title('The number of components needed to explain variance')
+
+    plt.axhline(y=0.95, color='r', linestyle='-')
+    plt.text(0.5, 0.85, '95% cut-off threshold', color='red', fontsize=16)
+
+    ax.grid(axis='x')
+    plt.show()
+
+    mlp = (MLPClassifier(hidden_layer_sizes=(neurons, hidden_layer), max_iter=1000,
+                         learning_rate_init=0.001))
+    mlp.fit(X_train, y_train)
+
+    X_pred = mlp.predict(X_test)
+
+    acc = accuracy_score(X_pred, y_test)
+    return X_pred, acc
+
+
+def mlp_gui_transformer(features, labels, test_size, pca_components, neurons, hidden_layer):
     print("TODO")
+
 
--- src/ml-bd-for-code-quality/inno3/ml-bd-for-cq.py
+++ src/ml-bd-for-code-quality/inno3/ml-bd-for-cq.py
@@ -1,7 +1,7 @@
 import PySimpleGUI as sg
 import PySimpleGUI_Events as sge
 import pandas as pd
-from lib import process_stack_trace_column, word_embedding, k_means, rfc, svm, mlp
+from lib import process_stack_trace_column, word_embedding, k_means_gui, rfc_gui, svm_gui, mlp_gui
 from sklearn.model_selection import train_test_split
 import time
 
@@ -191,29 +191,44 @@ def _compute_handler(values, application_data):
     if values[KMEANS_KEY]:
         # K-Means
         application_data.window[LOG_KEY].update(f"{application_data.window[LOG_KEY].get()}Clustering started. [K-Means] {time.time()-start}\n")
-        result = k_means(tf_idf, n_clusters=5)
+        result = k_means_gui(tf_idf, n_clusters=5)
         print(result)
         application_data.window[LOG_KEY].update(f"{application_data.window[LOG_KEY].get()}Clustering finished. [K-Means] {time.time()-start}\n")
 
     elif values[RFC_KEY]:
         # Random Forest
         application_data.window[LOG_KEY].update(f"{application_data.window[LOG_KEY].get()}Clustering started. [Random Forest Classifier] {time.time()-start}\n")
-        result = rfc(tf_idf, df[label_col], test_size=0.3, estimators=100, max_depth=100)
-        print(result)
+        if use_transformer:
+            #result = rfc_transformer_gui(df[label_col], model, )
+            #print(result)
+            print("not implemented yet")
+        else:
+            result = rfc_gui(tf_idf, df[label_col], test_size=0.3, estimators=100, max_depth=100)
+            print(result)
         application_data.window[LOG_KEY].update(f"{application_data.window[LOG_KEY].get()}Clustering finished. [Random Forest Classifier] {time.time()-start}\n")
 
     elif values[SVM_KEY]:
         # Support Vector Machine
         application_data.window[LOG_KEY].update(f"{application_data.window[LOG_KEY].get()}Clustering started. [Support Vector Machine] {time.time()-start}\n")
-        result = svm(tf_idf, df[label_col], test_size=0.3)
-        print(result)
+        if use_transformer:
+            #result = svm_transformer_gui(df[label_col], model, )
+            #print(result)
+            print("not implemented yet")
+        else:
+            result = svm_gui(tf_idf, df[label_col], test_size=0.3, kernel=values[KERNEL_TYPE])
+            print(result)
         application_data.window[LOG_KEY].update(f"{application_data.window[LOG_KEY].get()}Clustering finished. [Support Vector Machine] {time.time()-start}\n")
 
     elif values[MLP_KEY]:
         # Neural Network
         application_data.window[LOG_KEY].update(f"{application_data.window[LOG_KEY].get()}Clustering started. [Neural Network] {time.time()-start}\n")
-        result = mlp(tf_idf, df[label_col], test_size=0.3, pca_components=500, neurons=1000, hidden_layer=1)
-        print(result)
+        if use_transformer:
+            #result = mlp_transformer_gui(df[label_col], model, )
+            #print(result)
+            print("not implemented yet")
+        else:
+            result = mlp_gui(tf_idf, df[label_col], test_size=0.3, pca_components=500, neurons=1000, hidden_layer=1)
+            print(result)
         application_data.window[LOG_KEY].update(f"{application_data.window[LOG_KEY].get()}Clustering finished. [Neural Network] {time.time()-start}\n")
 
 compute_handler = sge.SimpleHandler(COMPUTE_KEY, _compute_handler)
